3b138fc7dace1c283b170e2010208691d46a9b3f
sarafasra <sarafasraoui75@gmail.com>
2026-04-16 10:38:46 +0100

    creer la table user avec les type et les foreignkey

--- structure.sql
+++ structure.sql
@@ -2,3 +2,11 @@ use  edusync;
 CREATE table roles (id int PRIMARY KEY AUTO_INCREMENT ,
 label VARCHAR (50) NOT NULL
 );
+CREATE table  users (id INT PRIMARY key AUTO_INCREMENT ,
+firstname VARCHAR (50) NOT NULL ,
+lastname VARCHAR (50) NOT NULL ,
+email VARCHAR (100) NOT NULL UNIQUE ,
+password VARCHAR (255) NOT NULL ,
+role_id INT NOT NULL , 
+Foreign Key (role_id) REFERENCES roles (id)
+);
