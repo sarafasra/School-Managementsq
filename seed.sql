5e656c04ec95291dd242e30e2cf216f993b52891
sarafasra <sarafasraoui75@gmail.com>
2026-04-16 11:57:30 +0100

    inserer la table courses

--- seed.sql
+++ seed.sql
@@ -7,3 +7,6 @@ VALUES ('sara','fasraoui','sara@gmail.com','sarhjk',1),
 INSERT INTO  classes (name,classroom_number)
 VALUES('développement stack','A1'),
 ('développement mobule','B2');
+INSERT INTO courses (title,description,total_hours,users_id)
+VALUES('java','introduction au web',30,3),
+('php','php avancer',40,2);
