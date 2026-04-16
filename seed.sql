e1423df142ae5b40cbfd9e23bcf95013fce8d663
sarafasra <sarafasraoui75@gmail.com>
2026-04-16 12:02:09 +0100

    inserer la table enrollement

--- seed.sql
+++ seed.sql
@@ -12,3 +12,6 @@ VALUES('java','introduction au web',30,3),
 ('php','php avancer',40,2);
 INSERT INTO students (dateofbirth,student_number,user_id,class_id)
 VALUES ('2020-04-02','STUD01',1,1);
+INSERT INTO enrollments (enrolled_at,status,student_id,course_id)
+VALUES ('2025-03-03','actif',1,2),
+('2025-08-05','actif',1,1);
