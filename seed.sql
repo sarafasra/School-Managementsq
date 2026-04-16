19dc449bf05b0e7540fccfdb42f4c8cbd4b2bc26
sarafasra <sarafasraoui75@gmail.com>
2026-04-16 11:50:55 +0100

    inserer la deuxieme table users

---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO roles (label)
+VALUES ('prof'),('student'),('admin');
+INSERT INTO users (firstname,lastname,email,password,role_id)
+VALUES ('sara','fasraoui','sara@gmail.com','sarhjk',1),
+('jihan','jador','jihan@gmail.com','fghhjj',2),
+('salwa','amzan','salwa@gmail.com','jkjkhk',3);
