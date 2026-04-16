3e73988a5bc94e7708a59f64311b142f4c08ffc9
sarafasra <sarafasraoui75@gmail.com>
2026-04-16 11:54:44 +0100

    inserer la table classes

--- seed.sql
+++ seed.sql
@@ -4,3 +4,6 @@ INSERT INTO users (firstname,lastname,email,password,role_id)
 VALUES ('sara','fasraoui','sara@gmail.com','sarhjk',1),
 ('jihan','jador','jihan@gmail.com','fghhjj',2),
 ('salwa','amzan','salwa@gmail.com','jkjkhk',3);
+INSERT INTO  classes (name,classroom_number)
+VALUES('développement stack','A1'),
+('développement mobule','B2');
