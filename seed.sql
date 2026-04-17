73d180a54ad6b3d9b0c0de3a2d91a4c0e0ee737f
sarafasra <sarafasraoui75@gmail.com>
2026-04-17 17:50:00 +0100

    modifier quelque  table

--- seed.sql
+++ seed.sql
@@ -11,24 +11,25 @@ VALUES
 ('Salma','Prof','salma@edusync.com','pass',2),
 ('Imane','Student','imane@edusync.com','pass',3),
 ('Hamza','Student','hamza@edusync.com','pass',3);
+SELECT * FROM classes ;
 INSERT INTO classes (name, classroom_number)
 VALUES
 ('Développement Web 2026','A1'),
 ('Développement Mobile 2026','B2');
 INSERT INTO courses (title, description, total_hours, users_id)
 VALUES
-('HTML/CSS','Bases du web',20,2),
-('JavaScript','JS avancé',40,2),
-('ReactJS','Frontend moderne',35,3);
+('HTML/CSS','Bases du web',20,4),
+('JavaScript','JS avancé',40,5),
+('ReactJS','Frontend moderne',35,6);
 INSERT INTO students (dateofbirth, student_number, user_id, class_id)
 VALUES
-('2001-03-10','STUD001',4,1),
-('2000-07-22','STUD002',5,1);
+('2001-03-10','STUD003',19,1),
+('2000-07-22','STUD004',6,1);
 INSERT INTO enrollments (enrolled_at, status, student_id, course_id)
 VALUES
-('2026-04-15','actif',1,1),
-('2026-04-15','actif',1,2),
-('2026-04-16','actif',2,1),
-('2026-04-16','actif',2,3);
+('2026-04-15','actif',1,7),
+('2026-04-15','actif',1,8),
+('2026-04-16','actif',2,9),
+('2026-04-16','actif',2,7);
 DELETE FROM courses
 WHERE id =2;
