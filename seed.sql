fea42f3fc2c4f940bcf074599d801251aa677007
sarafasra <sarafasraoui75@gmail.com>
2026-04-17 17:12:11 +0100

    ajouter des modification sur seed.sql insertion

--- seed.sql
+++ seed.sql
@@ -1,18 +1,34 @@
 INSERT INTO roles (label)
-VALUES ('prof'),('student'),('admin');
-INSERT INTO users (firstname,lastname,email,password,role_id)
-VALUES ('sara','fasraoui','sara@gmail.com','sarhjk',1),
-('jihan','jador','jihan@gmail.com','fghhjj',2),
-('salwa','amzan','salwa@gmail.com','jkjkhk',3);
-INSERT INTO  classes (name,classroom_number)
-VALUES('développement stack','A1'),
-('développement mobule','B2');
-INSERT INTO courses (title,description,total_hours,users_id)
-VALUES('java','introduction au web',30,3),
-('php','php avancer',40,2);
-INSERT INTO students (dateofbirth,student_number,user_id,class_id)
-VALUES ('2020-04-02','STUD01',1,1);
-INSERT INTO enrollments (enrolled_at,status,student_id,course_id)
-VALUES ('2025-03-03','actif',1,2),
-('2025-08-05','actif',1,1);
-SELECT * from users;
+VALUES 
+('admin'),
+('prof'),
+('student');
+
+INSERT INTO users (firstname, lastname, email, password, role_id)
+VALUES
+('Admin','System','admin@edusync.com','pass',1),
+('Youssef','Prof','youssef@edusync.com','pass',2),
+('Salma','Prof','salma@edusync.com','pass',2),
+('Imane','Student','imane@edusync.com','pass',3),
+('Hamza','Student','hamza@edusync.com','pass',3);
+INSERT INTO classes (name, classroom_number)
+VALUES
+('Développement Web 2026','A1'),
+('Développement Mobile 2026','B2');
+INSERT INTO courses (title, description, total_hours, users_id)
+VALUES
+('HTML/CSS','Bases du web',20,2),
+('JavaScript','JS avancé',40,2),
+('ReactJS','Frontend moderne',35,3);
+INSERT INTO students (dateofbirth, student_number, user_id, class_id)
+VALUES
+('2001-03-10','STUD001',4,1),
+('2000-07-22','STUD002',5,1);
+INSERT INTO enrollments (enrolled_at, status, student_id, course_id)
+VALUES
+('2026-04-15','actif',1,1),
+('2026-04-15','actif',1,2),
+('2026-04-16','actif',2,1),
+('2026-04-16','actif',2,3);
+DELETE FROM courses
+WHERE id =2;
--- structure.sql
+++ structure.sql
@@ -2,6 +2,7 @@ use  edusync;
 CREATE table roles (id int PRIMARY KEY AUTO_INCREMENT ,
 label VARCHAR (50) NOT NULL
 );
+DROP table roles ;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -16,31 +17,36 @@ CREATE TABLE users (
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 );
+
 CREATE TABLE classes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     classroom_number VARCHAR(20) NOT NULL
 );
+
 CREATE TABLE courses (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    description TEXT,
+    description varchar (50) NOT NULL,
     total_hours INT NOT NULL,
     users_id INT NOT NULL,
-        FOREIGN KEY (users_id)REFERENCES users(id)
+    FOREIGN KEY (users_id)
+        REFERENCES users(id)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 );
-
-
 CREATE TABLE students (
     id INT AUTO_INCREMENT PRIMARY KEY,
     dateofbirth DATE NOT NULL,
-    student_number VARCHAR(50) NOT NULL UNIQUE,
-    user_id INT NOT NULL UNIQUE,
-    class_id INT NOT NULL,
-        FOREIGN KEY (user_id)REFERENCES users(id),
-        FOREIGN KEY (class_id)REFERENCES classes(id)
+    student_number VARCHAR(50) UNIQUE NOT NULL,
+    user_id INT UNIQUE,
+    class_id INT,
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (class_id)
+        REFERENCES classes(id)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 );
@@ -48,14 +54,15 @@ CREATE TABLE students (
 CREATE TABLE enrollments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     enrolled_at DATE NOT NULL,
-    status ENUM('Actif','Terminé') NOT NULL,
-    student_id INT NOT NULL,
-    course_id INT NOT NULL,
-        FOREIGN KEY (student_id)REFERENCES students(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-        FOREIGN KEY (course_id)REFERENCES courses(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-  
+    status ENUM('actif','terminé') NOT NULL,
+    student_id INT,
+    course_id INT,
+    UNIQUE (student_id, course_id),
+    FOREIGN KEY (student_id)
+        REFERENCES students(id),
+     
+    FOREIGN KEY (course_id)
+        REFERENCES courses(id)
+       
 );
+DROP table enrollments;
